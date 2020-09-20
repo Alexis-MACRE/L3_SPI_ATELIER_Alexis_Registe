d44ae09c653bef1d50080ff5911adaf7069ff9b6
Thunder-web <59866108+Thunder-web@users.noreply.github.com>
2020-09-21 01:19:44 +0200

    ex4 et fonction de test

--- Atelier 4/exercice4.py
+++ Atelier 4/exercice4.py
@@ -8,6 +8,16 @@ from exercice2 import mots_Nlettres
 from exercice3 import dictionnaire
 
 def mot_correspond(mot:str,motif:str)->bool:
+     """Permet de vérifier si le mot peut être écrit à partir du motif donné.
+     ex: "cheval" et "c..v.l" retourne True
+         "cheval" et "c.v..l" retourne False
+     
+     Arguments:
+     mot -- le mot complet que l'on souhaite comparer
+     motif -- le mot ou les lettres joker ont étés remplacées par des "."
+     
+     Retourne True ou False selon le resultat du test.
+     """
      test_mot = list(mot)
      longueur_mot = len(mot)
 
@@ -24,22 +34,30 @@ def mot_correspond(mot:str,motif:str)->bool:
      return correspond
 
 def presente(lettre:str,mot:str)->int:
+     """Retourne la position de la lettre dans le mot"""
      return mot.find(lettre)
 
 
 def nombre_apparitions(char:str,mot:str)->int:
-          
-          compteur = 0
+     """Retourne le nombre d'apparitions de la lettre char dans un mot"""     
+     compteur = 0
 
-          for lettre in mot:
-               if lettre == char:
-                    compteur+=1
+     for lettre in mot:
+          if lettre == char:
+               compteur+=1
 
-          return compteur   
+     return compteur   
 
 
 def mot_possible(mot:str,lettres:str)->bool:
+     """Permet de savoir si l'on peut écrire le mot désigné avec les lettres que le joueur a. 
      
+     Arguments:
+     mot -- mot que l'on souhaite écrire
+     lettres -- liste des lettres possédées
+     
+     Retourne True si le joueur peut, False si le joueur ne peut pas
+     """
      possible = False
      compteur = 0
 
@@ -53,13 +71,25 @@ def mot_possible(mot:str,lettres:str)->bool:
      return possible
 
 def mot_optimaux(dico:list,lettres:str)->list:
+     """Prend une liste de mot et retourne les mots avec le plus de lettres que l'on peut faire avec les lettres possédées.
+     On ne garde que les mots dont la longueur est inférieure ou égale à la quantité de lettres que l'on a.
+     On liste parmi les mots restants ceux que l'on peut écrire tout en gardant en mémoire la plus grande longueur de mot de
+     cette liste.
+     On fini par retourner tous les mots de la liste qui sont de la longueur maximum.
+     
+     Arguments:
+     dico -- liste des mots
+     lettres -- lettres que l'on possède
+     
+     Retourne la liste des mots les plus longs écrivables avec les lettres possédées.
+     """
      liste_longueur_max = []
      liste_possible = []
 
      record_longueur=0
 
      for i in range(len(lettres)+1):
-          liste_longueur_max+=mots_Nlettres(dico,i)
+          liste_longueur_max+=mots_Nlettres(dico,i)    
      
      for mot in liste_longueur_max:
           if mot_possible(mot,lettres):
@@ -72,9 +102,8 @@ def mot_optimaux(dico:list,lettres:str)->list:
 
 dico = dictionnaire("littre.txt")
 
-print(mot_optimaux(dico,"chapeauoptty"))
-
 #test_fonction(mot_correspond,[["salut","s..u."],["mauvais","..m.."]],[True,False],2)
 #test_fonction(presente,[["t","salut"],["m","mauvais"]],[4,0],2)
 #test_fonction(nombre_apparitions,[["a","ananas"],["a","palace"],["a","pile"]],[3,2,0],2)
 #test_fonction(mot_possible,[["salut","asultazre"],["mauvais","oopipok"],["chapeau","chapeu"],["chapeau","uaepahc"]],[True,False,False,True],2)
+#test_fonction(mot_optimaux,[[dico,"paladin"],[dico,"chapeauoptty"],[dico,"eflzgrjgzzfref"]],[["paladin"],["autotype"],["greffer"]],2,False)
--- Atelier 4/fonctions_persos.py
+++ Atelier 4/fonctions_persos.py
@@ -1,31 +1,56 @@
+# Auteur(s) : MACRE Alexis
+# Date : 21/09/2020
+# Version : 3.0
+# Description : Fonction pour tester une fonction donnée
+
+
+def test_fonction(fonction:callable,valeurs:list,valeurs_attendues:list,nb_param:int=1,affichage_variable:bool=True) -> None :
+     """Fonction qui permet de tester une fonction pour chacune des valeurs passées en paramètre.
+     Elle compare les valeurs que la fonction retourne à celles qui sont attendues, puis affiche les résultats du test.
+     Obligation de préciser le nombre de paramètres de la fonction testée quand ils sont >=2.
+     Possibilité de désactiver l'affichage des valeurs d'entrées, dans le cas par exemple où cette dernière est très 
+     volumineuse (comme une liste de 15000 mots)
+     
+     Arguments:
+     fonction            -- la fonction à tester
+     valeurs             -- liste des paramètres en entrée pour chacun des tests
+     valeurs_attendues   -- liste des valeurs de retour attendues
+     nb_param            -- nombre de paramètres de la fonction, par défaut = 1
+     affichage           -- affichage ou non de la valeur en entrée
+     """
+
+     for i in range(len(valeurs)):                                              
+
+          if nb_param == 1:
+               resultat = fonction(valeurs[i]) 
+
+
+          elif nb_param == 2:
+               
+               resultat = fonction(valeurs[i][0],valeurs[i][1])
+
+
+          elif nb_param == 3:
+               resultat = fonction(valeurs[i][0],valeurs[i][1],valeurs[i][2])
+
+
+          elif nb_param == 4:
+               resultat = fonction(valeurs[i][0],valeurs[i][1],valeurs[i][2],valeurs[i][3])
+
+
+          if resultat!=valeurs_attendues[i]:
+               if affichage_variable:
+                    print("Test pour",valeurs[i],": Resultat Incorrect",resultat,"au lieu de",valeurs_attendues[i])
+               else:
+                    print("Test numero",i,": Resultat Incorrect",resultat,"au lieu de",valeurs_attendues[i])
+          else:
+               if affichage_variable:
+                    print("Test pour",valeurs[i],": Resultat Correct",resultat)
+               else:
+                    print("Test numero",i,": Resultat Correct",resultat) 
+
+
+
 # import sys
 # sys.path.append('../L3')
 # import testfonction
-
-def test_fonction(fonction:callable,valeurs:list,valeurs_attendues:list,nb_param:int=1) -> None :
-     """Permet de tester une fonction avec une liste de valeurs et celle des resultats attendus"""
-
-     for i in range(len(valeurs)) :
-          if nb_param == 1:
-               if fonction(valeurs[i])!=valeurs_attendues[i]:
-                    print("Test pour",valeurs[i],": Resultat Incorrect",fonction(valeurs[i]),"au lieu de",valeurs_attendues[i])
-               else:
-                    print("Test pour",valeurs[i],": Resultat Correct",fonction(valeurs[i]))
-
-          elif nb_param == 2:
-               if fonction(valeurs[i][0],valeurs[i][1])!=valeurs_attendues[i]:
-                    print("Test pour",valeurs[i],": Resultat Incorrect",fonction(valeurs[i][0],valeurs[i][1]),"au lieu de",valeurs_attendues[i])
-               else:
-                    print("Test pour",valeurs[i],": Resultat Correct",fonction(valeurs[i][0],valeurs[i][1]))
-          
-          elif nb_param == 3:
-               if fonction(valeurs[i][0],valeurs[i][1],valeurs[i][2])!=valeurs_attendues[i]:
-                    print("Test pour",valeurs[i],": Resultat Incorrect",fonction(valeurs[i][0],valeurs[i][1],valeurs[i][2]),"au lieu de",valeurs_attendues[i])
-               else:
-                    print("Test pour",valeurs[i],": Resultat Correct",fonction(valeurs[i][0],valeurs[i][1],valeurs[i][2]))
-
-          elif nb_param == 4:
-               if fonction(valeurs[i][0],valeurs[i][1],valeurs[i][2],valeurs[i][3])!=valeurs_attendues[i]:
-                    print("Test pour",valeurs[i],": Resultat Incorrect",fonction(valeurs[i][0],valeurs[i][1],valeurs[i][2],valeurs[i][3]),"au lieu de",valeurs_attendues[i])
-               else:
-                    print("Test pour",valeurs[i],": Resultat Correct",fonction(valeurs[i][0],valeurs[i][1],valeurs[i][2],valeurs[i][3]))
