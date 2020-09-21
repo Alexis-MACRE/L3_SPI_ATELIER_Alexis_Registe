40b87a4465b69da44601e1c9406a921cd9460954
Thunder-web <59866108+Thunder-web@users.noreply.github.com>
2020-09-21 02:38:51 +0200

    doctype exo1 et exo2

--- Atelier 4/exercice1.py
+++ Atelier 4/exercice1.py
@@ -1,12 +1,18 @@
-# Auteur(s) : 
-# Date : xx/xx/2020
+# Auteur(s) : MACRE Alexis & DJESSOU Registe
+# Date : 15/09/2020
 # Version : 1.0
-# Description : Atelier de programmation x Exo x Python
+# Description : Atelier de programmation 4 Exo 1 Python
 
 from fonctions_persos import test_fonction
 
 def full_name (str_arg:str)->str:
-     
+    """Prends une chaine "nom prenom" et la formate en "NOM Prenom"
+    
+    Arguments:
+    str_arg -- la chaine "non prenom"
+    
+    Retourne la chaine formatée
+    """   
     chaine_separee = str_arg.split()
     nom = chaine_separee[0]
     prenom = chaine_separee[1]
@@ -18,7 +24,13 @@ def full_name (str_arg:str)->str:
     return str_arg
 
 def is_mail(str_arg:str)->(int,int):
-
+     """Fonction de test de validité d'une adresse mail sous le format xxxxxx@univ-corse.fr
+     
+     Arguments:
+     str_arg -- la chaine de caractère à tester
+     
+     Retourne le couple (validité de l'adresse, code d'erreur)
+     """
      erreur = 0
 
      position_arobase = str_arg.find("@")
@@ -46,17 +58,19 @@ def is_mail(str_arg:str)->(int,int):
           erreur = 3
 
 
-     validite = max(0,1-erreur)
+     validite = (erreur==0)
 
      return (validite,erreur)
 
-def message_erreur_exo1(code_erreur):
+
+def message_erreur_exo1(code_erreur:int)->str:
+     """Fonction qui retourne le message d'erreur correspondant au code d'erreur"""
      message_erreur = ["le mail est valide", "le corps n'est pas valide","il manque l'arobase","le domaine n'est pas valide","il manque le point après le domaine"]
      return message_erreur[code_erreur]
 
-retour = is_mail("aaa@univ-corse.fr")
-
-print(message_erreur_exo1(retour[1]))
-
 #test_fonction(full_name,["aa bb","cc dd"],["AA Bb","CC Dd"])
 #test_fonction(is_mail,["a aa@univ-corse.fr","aaaaauniv-corse.fr","aaaaaa@gmail.com","aaaa@univ-corsefr","valide@univ-corse.fr"],[(0,1),(0,2),(0,3),(0,4),(1,0)],2)
+
+#retour = is_mail("aaa@univ-corse.fr")
+
+#print(message_erreur_exo1(retour[1]))
--- Atelier 4/exercice2.py
+++ Atelier 4/exercice2.py
@@ -1,4 +1,11 @@
+# Auteur(s) : MACRE Alexis & DJESSOU Registe
+# Date : 15/09/2020
+# Version : 1.0
+# Description : Atelier de programmation 4 Exo 2 Python
+
+
 def mots_Nlettres(lst_mot:list, n:int)->list:
+    """Prend une liste de mot et retourne la liste composée des mots de longueur n"""
     taille = len(lst_mot)
     i = 0
     lst_final = []
@@ -10,17 +17,21 @@ def mots_Nlettres(lst_mot:list, n:int)->list:
 
 
 def text_exercice1():
+    """Fonction de test de la consigne"""
     lst_mot = ["jouer","bonjour","punir","jour","aurevoir","revoir","pouvoir","cour","abajour","finir","aimer"]
     return mots_Nlettres(lst_mot,5)
 
 def commence_par(mot:str,prefixe:str)->bool:
+    """Vérifie si mot commence par préfixe"""
     return (mot.find(prefixe) == 0)
 
 def finit_par(mot:str,suffixe:str)->bool:
+    """Vérifie si mot fini par suffixe"""
     index_suffixe = len(mot) - len(suffixe)
     return (mot.rfind(suffixe) == index_suffixe)
 
 def finissent_par(lst_mot:str,suffixe:str)->list:
+    """Prend une liste de mot et retourne la liste composée de ceux qui finissent par suffixe"""
     liste_retour = []
     for mot in lst_mot:
         if finit_par(mot,suffixe):
@@ -29,6 +40,7 @@ def finissent_par(lst_mot:str,suffixe:str)->list:
     return liste_retour
 
 def commencent_par(lst_mot:str,prefixe:str)->list:
+    """Prend une liste de mot et retourne la liste composée de ceux qui commencent par préfixe"""
     liste_retour = []
     for mot in lst_mot:
         if commence_par(mot,prefixe):
@@ -38,6 +50,8 @@ def commencent_par(lst_mot:str,prefixe:str)->list:
 
 
 def liste_mots(lst_mot:str,prefixe:str,suffixe:str,n:int)->list:
+    """Prend une liste de mot et retourne la liste composée des mots de longueur n commençants par préfixe et
+    finissants par suffixe"""
     liste_commencent = commencent_par(lst_mot,prefixe)
     liste_finissent = finissent_par(liste_commencent,suffixe)
     liste_retour = mots_Nlettres(liste_finissent,n)
@@ -45,6 +59,14 @@ def liste_mots(lst_mot:str,prefixe:str,suffixe:str,n:int)->list:
     return liste_retour
 
 def dictionnaire(fichier:str):
+    """Transforme un fichier donné en un tableau de str qui correspondent à chacune des lignes. 
+    Note: Le fichier doit être dans le même dossier que ce code
+
+    Arguments:
+    fichier -- nom du fichier à ouvrir
+
+    Retourne la liste de str.
+    """
     mon_fichier = open(fichier, 'r', encoding='utf8')
     liste_lignes = mon_fichier.readlines()
     return [ligne.strip("\n") for ligne in liste_lignes]
